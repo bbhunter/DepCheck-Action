b270d7a68fe8797ea2bdab497de144252fde4184
Jerry Gamblin <jerry.gamblin@gmail.com>
2018-12-28 08:11:21 -0600

    Project Name
    Project Name

--- DepCheck/entrypoint.sh
+++ DepCheck/entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+projectname="DepCheck Action"
 
-/usr/share/dependency-check/bin/dependency-check.sh --enableExperimental  --format JSON --scan /
+
+/usr/share/dependency-check/bin/dependency-check.sh --project $projectname --enableExperimental  --format JSON --scan /
+
+
+#curl -i -H 'Authorization: token '$Github_Auth_Token''  -H "Content-Type: application/json" -X POST --data '{"title":"'"$name"'", "body":"'"$description"'"}' $Github_Repo_URL
