7d6c826befb909264dd18db995034c1c04b6c72b
Jerry Gamblin <jerry.gamblin@gmail.com>
2018-12-28 10:00:01 -0600

    Try CSV
    Try CSV

--- DepCheck/entrypoint.sh
+++ DepCheck/entrypoint.sh
@@ -2,11 +2,11 @@
 projectname="DepCheck Action"
 
 /dependency-check/bin/dependency-check.sh --update
-/dependency-check/bin/dependency-check.sh --project $projectname --enableExperimental  --format JSON --format VULN --scan /
+/dependency-check/bin/dependency-check.sh --project $projectname --enableExperimental  --format CSV --format VULN --scan /
 
 
 ls -l
 
-description=$(cat *.json)
+description=$(cat *.CSV)
 
 curl -i -H 'Authorization: token '$GITHUB_TOKEN''  -H "Content-Type: application/json" -X POST --data '{"title":"'"Report Vuln"'", "body":"'"$description"'"}' https://api.github.com/repos/jgamblin/DepCheck-Action/issues
