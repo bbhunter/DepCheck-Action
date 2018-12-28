6bd4e5fbe3af9c3e6f25de1eaea67d968e02702e
Jerry Gamblin <jerry.gamblin@gmail.com>
2018-12-28 10:46:55 -0600

    Test
    Test

--- DepCheck/entrypoint.sh
+++ DepCheck/entrypoint.sh
@@ -4,5 +4,5 @@ projectname="DepCheck Action"
 bundle-audit update
 
 /dependency-check/bin/dependency-check.sh --update
-/dependency-check/bin/dependency-check.sh --project $projectname --format VULN --scan /
-/dependency-check/bin/dependency-check.sh --project $projectname --format VULN --failOnCVSS 7 --scan /
+/dependency-check/bin/dependency-check.sh --project $projectname --scan /
+/dependency-check/bin/dependency-check.sh --project $projectname --failOnCVSS 7 --scan /
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# DepCheck-Action
+# Dependency Check Github Action
+
+A POC Github Action to check all dependencies in Ruby and Node packages and to fail if any of them have a CVSS above 7.  
+
+The Gemfile is from the latest rails build.
