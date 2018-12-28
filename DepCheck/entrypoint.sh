8b215dea68e68b416e00b15ac30594c84a70fb9f
Jerry Gamblin <jerry.gamblin@gmail.com>
2018-12-28 10:41:18 -0600

    Update entrypoint.sh

--- DepCheck/entrypoint.sh
+++ DepCheck/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 projectname="DepCheck Action"
 
+bundle-audit update
+
 /dependency-check/bin/dependency-check.sh --update
+/dependency-check/bin/dependency-check.sh --project $projectname --format VULN --scan /
 /dependency-check/bin/dependency-check.sh --project $projectname --format VULN --failOnCVSS 7 --scan /
-
-
-#curl -i -H 'Authorization: token '$GITHUB_TOKEN''  -H "Content-Type: application/json" -X POST --data '{"title":"'"Report Vuln"'", "body":"'"$description"'"}' https://api.github.com/repos/jgamblin/DepCheck-Action/issues
