5f029300dc174a278564d398f909920636337929
Jerry Gamblin <jerry.gamblin@gmail.com>
2018-12-28 10:32:17 -0600

    Update entrypoint.sh

--- DepCheck/entrypoint.sh
+++ DepCheck/entrypoint.sh
@@ -2,16 +2,7 @@
 projectname="DepCheck Action"
 
 /dependency-check/bin/dependency-check.sh --update
-/dependency-check/bin/dependency-check.sh --project $projectname --enableExperimental  --format CSV --format VULN --scan /
+/dependency-check/bin/dependency-check.sh --project $projectname --format VULN --failOnCVSS 7 --scan /
 
 
-ls -l
-
-cat *.csv > temp.csv
-awk '!x[$0]++' temp.csv > temp2.csv
-cut -d',' -f1-4,6- temp2.csv > githubvulns.csv
-cat githubvulns.csv
-
-description=$(cat githubvulns.csv)
-
-curl -i -H 'Authorization: token '$GITHUB_TOKEN''  -H "Content-Type: application/json" -X POST --data '{"title":"'"Report Vuln"'", "body":"'"$description"'"}' https://api.github.com/repos/jgamblin/DepCheck-Action/issues
+#curl -i -H 'Authorization: token '$GITHUB_TOKEN''  -H "Content-Type: application/json" -X POST --data '{"title":"'"Report Vuln"'", "body":"'"$description"'"}' https://api.github.com/repos/jgamblin/DepCheck-Action/issues
