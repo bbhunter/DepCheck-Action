73f3ff10572417d5d5f1298551d0e7820c53c3a7
Jerry Gamblin <jerry.gamblin@gmail.com>
2018-12-28 10:09:37 -0600

    More Testing
    More Testing.

--- DepCheck/entrypoint.sh
+++ DepCheck/entrypoint.sh
@@ -7,6 +7,8 @@ projectname="DepCheck Action"
 
 ls -l
 
-description=$(cat *.CSV)
+cat dependency-check-report.csv
+
+description=$(cat dependency-check-report.csv)
 
 curl -i -H 'Authorization: token '$GITHUB_TOKEN''  -H "Content-Type: application/json" -X POST --data '{"title":"'"Report Vuln"'", "body":"'"$description"'"}' https://api.github.com/repos/jgamblin/DepCheck-Action/issues
