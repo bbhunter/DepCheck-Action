b95183da6ff9a73e22c662e5f4baa912e44efc4c
Jerry Gamblin <jerry.gamblin@gmail.com>
2018-12-28 10:56:30 -0600

    Update entrypoint.sh

--- DepCheck/entrypoint.sh
+++ DepCheck/entrypoint.sh
@@ -3,6 +3,4 @@ projectname="DepCheck Action"
 
 bundle-audit update
 
-/dependency-check/bin/dependency-check.sh --update
-/dependency-check/bin/dependency-check.sh --project $projectname --scan /
-/dependency-check/bin/dependency-check.sh --project $projectname --failOnCVSS 7 --scan /
+/dependency-check/bin/dependency-check.sh --update --project $projectname --scan /
